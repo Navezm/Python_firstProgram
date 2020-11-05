0dedd491827c767dae2773d34b3931e29373af58
Navezm <navez.martin@gmail.com>
2020-11-05 17:18:40 +0100

    Part 1

---
+++ main.py
@@ -0,0 +1,14 @@
+quotes = [
+    "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !",
+    "On doit pouvoir choisir entre s'écouter parler et se faire entendre"
+]
+
+characters = [
+    "Alvin et les Chipmunks",
+    "Babar",
+    "Betty Boop",
+    "Calimero",
+    "Casper",
+    "Le chat Potté",
+    "Kirikou"
+]
