34be5d760d286f6dd0d8bc4c3ec1a59e12640176
Navezm <navez.martin@gmail.com>
2021-02-16 16:23:38 +0100

    Add JSON files

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import random
 import json
 
-quotes = [
-    "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !",
-    "On doit pouvoir choisir entre s'écouter parler et se faire entendre"
-]
+# quotes = [
+#     "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !",
+#     "On doit pouvoir choisir entre s'écouter parler et se faire entendre"
+# ]
 
 # characters = [
 #     "alvin et les Chipmunks",
@@ -17,35 +17,35 @@ quotes = [
 # ]
 
 # Read value from json file
-def read_value_from_json():
+def read_values_from_json():
     values = [] # create a new empty list
     with open('characters.json') as f:
         data = json.load(f) # load the data from that file. data = entries
         for entry in data :
             values.append(entry['characters']) # add each element in my list
         return values # return my complete list
-        
+
 def message(character, quote):
-    capitalize(character)
-    capitalize(quote)
-    return "{} a dit : {}".format(character, quote)
+    n_character = character.capitalize()
+    n_quote = quote.capitalize()
+    return "{} a dit : {}".format(n_character, n_quote)
 
-def random_character():
-    all_values = read_value_from_json()
-    return show_random_quote(all_values)
+def get_random_item_in(my_list):
+    rand_numb = random.randint(0, len(my_list) - 1)
+    item = my_list[rand_numb] # get a quote from a list
+    return item # return the item
 
-def show_random_quote(my_list):
-    rand_numb = random.randint(0, len(my_list) -1) # random number
-    item = my_list[rand_numb] # quote from the list
-    return item
+def get_random_quote():
+    all_values = read_values_from_json('quotes.json', 'quote')
+    return get_random_item_in(all_values)
 
-def capitalize(words):
-    for item in words:
-        item.capitalize()
+def get_random_character():
+    all_values = read_values_from_json('characters.json', 'character')
+    return get_random_item_in(all_values)
 
-
-user_answer = input("Tapez entrée pour connaitre une autre citation ou B pour quitter le programme.")
+# Programm
+user_answer = input('Tapez entrée pour connaître une autre citation ou B pour quitter le programme.')
 
 while user_answer != "B":
-    print(message(random_character(), show_random_quote(quotes))
-    user_answer = input("Tapez entrée pour connaitre une autre citation ou B pour quitter le programme.")
+    print(message(get_random_character(), get_random_quote()))
+    user_answer = input('Tapez entrée pour connaître une autre citation ou B pour quitter le programme.')
